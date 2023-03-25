2c72c80a23eb8398b880ec7ef2582e49703042b0
sabbir ahammad <113548902+sabbirahammad@users.noreply.github.com>
2023-03-26 00:18:59 +0600

    syncro use for node-js

---
+++ class3 syncro/index.js
@@ -0,0 +1,17 @@
+let os=require('os');
+
+//nijer pc all detils(os=oparating system)
+
+
+//console.log(os.userInfo());
+//console.log(os.homedir());
+// console.log(os.totalmem());
+// console.log(os.freemem());
+
+const path=require('path');
+// console.log(path);
+
+// const extname=path.extname('index.cpp');
+// console.log(extname);
+// const joined=path.join(__dirname + "/overviews");
+// console.log(joined);
